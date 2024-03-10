830d25e89d9f3d10a9f75fd0f093e5dbb7643211
Shigueru Nagata <shiguerunagata@protonmail.com>
2024-03-10 19:41:44 -0300

    Primer script. Paquetes para compilar en Debian.

---
+++ scripts_debian_compilar.sh
@@ -0,0 +1,25 @@
+# Autor: Shigueru Nagata
+# Fecha: 10-03-2024
+# Descripción:
+'
+Script para instalar paquetes necesarios para
+compilar en Debian. Tambien se instalan paquetes
+necearios para algunos programas de simulación
+pre-compilados y que pueden estar dinamicamente
+enlazados.
+'
+# Paquetes instalados
+'
+- Openblas librerias de desarrollo
+- Lapack librerias de desarrollo
+- fftw3 librerias de desarrollo
+- Compilador Gfortran
+- Compilador de c++ g++
+- OpenMPI librerias de desarrollo
+- MPI librerias de desarrollo
+'
+Pkgs="libopenblas-dev liblapack-dev libfftw3-dev gfortran g++ libopenmpi-dev mpi-default-dev make cmake"
+
+# Comando
+
+sudo apt install --no-install-recommends Pkgs
